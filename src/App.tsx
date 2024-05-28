8e47bd6670c1ff7c7a0402c4d257ab89223aa8df
lasse <77295879+lassejlv@users.noreply.github.com>
2024-05-28 09:12:31 +0200

    changes lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import "./App.scss";
+import "./styles/app.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const plusCount = count > 100 ? Math.floor(Math.random() * 5000) : 10;
 
   useEffect(() => {
     const localCount = Number(localStorage.getItem("count")) || 0;
@@ -13,13 +14,24 @@ function App() {
     <>
       <div className="card">
         <button
+          className="btn"
           onClick={() => {
-            const newCount = count + 1;
+            const newCount = count + plusCount;
             setCount(newCount);
             localStorage.setItem("count", newCount.toString());
           }}
         >
           Count is {count.toLocaleString()}
+        </button>
+
+        <button
+          className="btn-danger"
+          onClick={() => {
+            setCount(0);
+            localStorage.setItem("count", "0");
+          }}
+        >
+          Reset
         </button>
       </div>
     </>
